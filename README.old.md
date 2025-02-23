6960c4320bccc06732698260a097a987c48f9b70
ZGen88 <zgen88@uw.edu>
2025-02-22 16:58:41 -0800

    Updates

--- README.old.md
+++
@@ -1,2 +0,0 @@
-# Team_Cedar
-Code repository for UW INFO 490 Capstone group NOAAxNWIFC
--- src/cosineCalc.js
+++
@@ -1,9 +0,0 @@
-const dotProduct = (vecA, vecB) => vecA.reduce((sum, a, i) => sum + a * vecB[i], 0);
-
-const magnitude = (vec) => Math.sqrt(vec.reduce((sum, val) => sum + val * val, 0));
-
-export const cosineCalc = (vecA, vecB) => {
-  const magA = magnitude(vecA);
-  const magB = magnitude(vecB);
-  return magA && magB ? dotProduct(vecA, vecB) / (magA * magB) : 0;
-};
